a5dac512198f94d8382f956e3dbb7d006f63410c
Schadrack Cineas <sjcineas@gmail.com>
2023-03-20 01:33:37 -0400

    Initial Commit

---
+++ scavenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scavenger
+//
+//  Created by Schadrack Cineas on 3/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
